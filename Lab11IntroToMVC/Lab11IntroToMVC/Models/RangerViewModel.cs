f25649ff96283257922f30067615579742a93ce5
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-18 16:10:09 -0500

    modifying model to include filter functionality

--- Lab11IntroToMVC/Lab11IntroToMVC/Models/RangerViewModel.cs
+++ Lab11IntroToMVC/Lab11IntroToMVC/Models/RangerViewModel.cs
@@ -5,7 +5,18 @@ using System.Threading.Tasks;
 
 namespace Lab11IntroToMVC.Models
 {
-    public class Class
+    public class RangerViewModel
     {
+        public int firstYear { get; set; }
+        public int lastYear { get; set; }
+
+        public object[] RangeFinder(int firstYeat, int lastYear)
+        {
+            for(int i = firstYear; i < lastYear; i++)
+            {
+
+            }
+            return 
+        } 
     }
 }
