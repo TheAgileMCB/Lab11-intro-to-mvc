e0342c536948815c29a77509178c924779eab864
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-18 16:09:15 -0500

    wrote routes to connect HomeController to model

--- Lab11IntroToMVC/Lab11IntroToMVC/Controllers/HomeController.cs
+++ Lab11IntroToMVC/Lab11IntroToMVC/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lab11IntroToMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lab11IntroToMVC.Controllers
---
+++ Lab11IntroToMVC/Lab11IntroToMVC/Models/RangerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab11IntroToMVC.Models
+{
+    public class Class
+    {
+    }
+}
