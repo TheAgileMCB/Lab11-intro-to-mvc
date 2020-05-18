20c0dcbdfb1977ca0c9435df453c8cf66db449b2
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-18 16:28:45 -0500

    Modified content to be in line with project

--- Lab11IntroToMVC/Lab11IntroToMVC/Models/RangerViewModel.cs
+++ Lab11IntroToMVC/Lab11IntroToMVC/Models/RangerViewModel.cs
@@ -10,13 +10,13 @@ namespace Lab11IntroToMVC.Models
         public int firstYear { get; set; }
         public int lastYear { get; set; }
 
-        public object[] RangeFinder(int firstYeat, int lastYear)
-        {
-            for(int i = firstYear; i < lastYear; i++)
-            {
+        //public object[] RangeFinder(int firstYeat, int lastYear)
+        //{
+        //    for(int i = firstYear; i < lastYear; i++)
+        //    {
 
-            }
-            return 
-        } 
+        //    }
+        //    return 
+        //} 
     }
 }
