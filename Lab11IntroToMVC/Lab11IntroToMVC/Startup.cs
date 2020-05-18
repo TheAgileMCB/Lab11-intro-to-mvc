3286c68176a8ec9f0b066e96a1635d95f2f616d7
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-18 15:13:51 -0500

    modified startup.cs file as the guide directs

--- Lab11IntroToMVC/Lab11IntroToMVC/Startup.cs
+++ Lab11IntroToMVC/Lab11IntroToMVC/Startup.cs
@@ -16,6 +16,7 @@ namespace Lab11IntroToMVC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,11 +31,11 @@ namespace Lab11IntroToMVC
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
             });
+
+            app.UseStaticFiles();
+
         }
     }
 }
