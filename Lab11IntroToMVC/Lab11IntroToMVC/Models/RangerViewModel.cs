712c1843598c2798e965aec78cb9e0ddd6c7935f
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-18 22:49:22 -0500

    lost here. Have probably taken myself further from the correct answer.

--- Lab11IntroToMVC/Lab11IntroToMVC/Models/RangerViewModel.cs
+++ Lab11IntroToMVC/Lab11IntroToMVC/Models/RangerViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,10 +7,12 @@ using static Lab11IntroToMVC.Models.Person;
 
 namespace Lab11IntroToMVC.Models
 {
-    public class RangerViewModel
+    public class RangerViewModel : IEnumerable
     {
         public int firstYear { get; set; }
         public int lastYear { get; set; }
+        int count;
+        T items;
 
         public void RangeFinder(int firstYear, int lastYear)
         {
@@ -19,5 +22,21 @@ namespace Lab11IntroToMVC.Models
             }
           
         }
+
+        public IEnumerator<Person> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return person[i];
+            }
+        }
+
+        // Non-generic, just do this because we have to
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
+
+   
 }
